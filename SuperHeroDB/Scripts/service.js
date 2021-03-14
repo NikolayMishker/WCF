0fbce55d8b1e61331eda32d46df08aaa1666b301
Nikolay Vinogradov <NikolayVinogradov15@yandex.ru>
2021-03-14 20:33:51 +0300

    WCF app

---
+++ SuperHeroDB/Scripts/service.js
@@ -0,0 +1,15 @@
+﻿function getAllHeroes() {
+    $.ajax(
+        {
+            url: "Service/SuperHeroService.svc/GetAllHeroes",
+            type: "GET",
+            dataType: "json",
+            success: function (result) {
+                heroes = result;
+                drawHeroTable(result);
+                console.log(result);
+            }
+
+        }
+    );
+}
---
+++ SuperHeroDB/Service/SuperHeroService.svc.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace SuperHeroDB.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class SuperHeroService
+    {
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public string DoWork()
+        {
+            return "This is the SuperHeroDB service!";
+        }
+
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<SuperHero> GetAllHeroes()
+        {
+            return Data.SuperHeroes;
+        }
+
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetHero/{id}") ]
+        public SuperHero GetHero(string id)
+        {
+            return Data.SuperHeroes.Find(hero => hero.Id == int.Parse(id));
+        }
+    }
+}
